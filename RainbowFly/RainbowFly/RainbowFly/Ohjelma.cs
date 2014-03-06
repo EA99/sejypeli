ecac1089f7d85083db1d166c24bf2f218fa8c5e0
EA99 <ilpo.kempas@seamk.fi>
2014-03-06 08:27:43 -0800

    Rainbowfly ensimmäienn commit

---
+++ RainbowFly/RainbowFly/RainbowFly/Ohjelma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+static class Ohjelma
+{
+#if WINDOWS || XBOX
+    static void Main(string[] args)
+    {
+        using (RainbowFly game = new RainbowFly())
+        {
+#if !DEBUG
+            game.IsFullScreen = true;
+#endif
+            game.Run();
+        }
+    }
+#endif
+}
---
+++ RainbowFly/RainbowFly/RainbowFly/RainbowFly.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class RainbowFly : PhysicsGame
+{
+    public override void Begin()
+    {
+        hahmoluo();
+        luoraha();
+        Image bgImg = LoadImage("tausta");
+        Level.Size = new Vector(bgImg.Width * 10, bgImg.Height);
+        Level.Background.Image = LoadImage("tausta");
+        Level.Background.TileToLevel();
+        Level.Background.MoveTo(new Vector(-Screen.Width, 0), 100);
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+    void hahmoluo()
+    {
+        PhysicsObject hahmo = new PhysicsObject(40, 65);
+        hahmo.Shape = Shape.Circle;
+        Add(hahmo);
+        Camera.Follow(hahmo);
+        hahmo.Velocity = new Vector(200, 0);
+        hahmo.CanRotate = false;
+
+    }
+    void luoraha()
+    {
+        PhysicsObject raha = new PhysicsObject(20, 20);
+        raha.Color = Color.Yellow;
+        raha.Shape = Shape.Circle;
+        raha.Position = RandomGen.NextVector(100, 400);
+        Add(raha);
+
+    }
+}
