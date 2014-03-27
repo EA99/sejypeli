a730d6922b0abef3bf7df1095090229f69ac94d5
EA99 <ilpo.kempas@seamk.fi>
2014-03-27 18:22:48 +0200

    Tehty laskuri joka laskee kerättyjä kolikkoja.

--- RainbowFly/RainbowFly/RainbowFly/RainbowFly.cs
+++ RainbowFly/RainbowFly/RainbowFly/RainbowFly.cs
@@ -11,16 +11,21 @@ public class RainbowFly : PhysicsGame
     GameObject sateenkaari;
     PhysicsObject hahmo;
     Vector lanta;
+    IntMeter pisteLaskuri;
+    
     public override void Begin()
     {
+        
         hahmoluo();
         luoraha();
+        LuoPistelaskuri();
+
         Image bgImg = LoadImage("tausta");
         Level.Size = new Vector(bgImg.Width * 50, bgImg.Height);
         Level.Background.Image = LoadImage("tausta");
         Level.Background.TileToLevel();
         Level.Background.MoveTo(new Vector(-Screen.Width, 0), 100);
-
+        
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Down, ButtonState.Down, liial,"",new Vector(0,-140));
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
@@ -42,7 +47,7 @@ public class RainbowFly : PhysicsGame
         sateenkaari= new GameObject(1,65);
         Add(sateenkaari,-1);
         sateenkaari.Image = LoadImage("sateenkaari");
-
+        
     }
 
     protected override void Update(Time time)
@@ -55,11 +60,15 @@ public class RainbowFly : PhysicsGame
 
     void luoraha()
     {
-        PhysicsObject raha = new PhysicsObject(20, 20);
+        IPhysicsObject raha=new PhysicsObject(20, 20);
         raha.Color = Color.Yellow;
         raha.Shape = Shape.Circle;
-        raha.Position = RandomGen.NextVector(100, 400);
+        //raha.Position = RandomGen.NextVector(100, 400);
+        raha.Position = Level.GetRandomPosition();
+        raha.IgnoresGravity = true;
         Add(raha);
+        AddCollisionHandler(hahmo, raha, rahsaa);
+        Timer.SingleShot(0.2, luoraha);
     }
     
     void liial(Vector suunta)
@@ -70,5 +79,24 @@ public class RainbowFly : PhysicsGame
     {
         hahmo.Velocity = new Vector(
             hahmo.Velocity.X, 0 );
+    }
+    void rahsaa(IPhysicsObject hahmo,IPhysicsObject raha)
+    {
+        raha.Destroy();
+        pisteLaskuri.Value += 100;
+    }
+    void LuoPistelaskuri()
+    {
+        pisteLaskuri = new IntMeter(0);
+
+        Label pisteNaytto = new Label(100,100);
+        pisteNaytto.X = Screen.Left + 100;
+        pisteNaytto.Y = Screen.Top - 100;
+        pisteNaytto.TextColor = Color.Black;
+        pisteNaytto.Color = Color.White;
+        pisteNaytto.TextScale = new Vector(2,2);
+
+        pisteNaytto.BindTo(pisteLaskuri);
+        Add(pisteNaytto);
     }
 }
